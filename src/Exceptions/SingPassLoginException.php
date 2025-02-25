ceb299c96d5e5fc7b2e98cbb6b36f62d32bf5cb8
SJC <shaun@accredify.io>
2025-02-25 16:57:49 +0800

    Updated redirection route to use named route 'login' and updated default error message

--- src/Exceptions/SingPassLoginException.php
+++ src/Exceptions/SingPassLoginException.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SingPassLoginException extends HttpException
 {
-    public function __construct(int $statusCode = 400, string $message = 'User not found.', ?Exception $previous = null, array $headers = [], int $code = 0)
+    public function __construct(int $statusCode = 400, string $message = 'This SingPass account is not connected with any existing accounts in our system.', ?Exception $previous = null, array $headers = [], int $code = 0)
     {
         parent::__construct($statusCode, $message, $previous, $headers, $code);
     }
@@ -18,11 +18,11 @@ class SingPassLoginException extends HttpException
      */
     public function render(): RedirectResponse
     {
-        return redirect()->back()->withErrors(
+        return redirect()->route('login')->withErrors(
             [
                 'singpass' => [
                     [
-                        'title' => 'SingPass Login Error',
+                        'title' => 'No Account Found',
                         'description' => $this->message,
                     ],
                 ],
