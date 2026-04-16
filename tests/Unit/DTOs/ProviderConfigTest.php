00865895a26428d5aca18679b67c5df116c186d9
SJC <shaun@accredify.io>
2026-04-16 20:00:41 +0800

    Add tests for ProviderConfig handling of login scopes and exceptions

--- tests/Unit/DTOs/ProviderConfigTest.php
+++ tests/Unit/DTOs/ProviderConfigTest.php
@@ -124,4 +124,107 @@ class ProviderConfigTest extends TestCase
 
         ProviderConfig::singPassMyInfo();
     }
+
+    public function test_singpass_login_accepts_empty_login_scopes(): void
+    {
+        Config::set('singpass-login.discovery_endpoint', 'https://example.com/discovery');
+        Config::set('singpass-login.client_id', 'test-client-id');
+        Config::set('singpass-login.redirect_uri', 'https://example.com/callback');
+        Config::set('singpass-login.domain', 'https://example.com');
+        Config::set('singpass-login.login_scopes', []);
+
+        $config = ProviderConfig::singPassLogin();
+
+        $this->assertSame([], $config->availableScopes);
+        $this->assertSame([], $config->loginScopes);
+    }
+
+    public function test_singpass_login_accepts_null_login_scopes(): void
+    {
+        Config::set('singpass-login.discovery_endpoint', 'https://example.com/discovery');
+        Config::set('singpass-login.client_id', 'test-client-id');
+        Config::set('singpass-login.redirect_uri', 'https://example.com/callback');
+        Config::set('singpass-login.domain', 'https://example.com');
+        Config::set('singpass-login.login_scopes', null);
+
+        $config = ProviderConfig::singPassLogin();
+
+        $this->assertSame([], $config->availableScopes);
+        $this->assertSame([], $config->loginScopes);
+    }
+
+    public function test_corppass_throws_when_available_scopes_is_not_array(): void
+    {
+        Config::set('corppass-login.discovery_endpoint', 'https://corppass.example.com/discovery');
+        Config::set('corppass-login.client_id', 'cp-client-id');
+        Config::set('corppass-login.redirect_uri', 'https://example.com/cp-callback');
+        Config::set('corppass-login.domain', 'https://corppass.example.com');
+        Config::set('corppass-login.available_scopes', 'openid');
+        Config::set('corppass-login.login_scopes', ['openid']);
+
+        $this->expectException(MissingConfigException::class);
+        $this->expectExceptionMessage('corppass-login.available_scopes');
+
+        ProviderConfig::corpPass();
+    }
+
+    public function test_corppass_throws_when_login_scopes_is_not_array(): void
+    {
+        Config::set('corppass-login.discovery_endpoint', 'https://corppass.example.com/discovery');
+        Config::set('corppass-login.client_id', 'cp-client-id');
+        Config::set('corppass-login.redirect_uri', 'https://example.com/cp-callback');
+        Config::set('corppass-login.domain', 'https://corppass.example.com');
+        Config::set('corppass-login.available_scopes', ['openid']);
+        Config::set('corppass-login.login_scopes', true);
+
+        $this->expectException(MissingConfigException::class);
+        $this->expectExceptionMessage('corppass-login.login_scopes');
+
+        ProviderConfig::corpPass();
+    }
+
+    public function test_corppass_throws_when_available_scopes_contains_non_string(): void
+    {
+        Config::set('corppass-login.discovery_endpoint', 'https://corppass.example.com/discovery');
+        Config::set('corppass-login.client_id', 'cp-client-id');
+        Config::set('corppass-login.redirect_uri', 'https://example.com/cp-callback');
+        Config::set('corppass-login.domain', 'https://corppass.example.com');
+        Config::set('corppass-login.available_scopes', ['openid', 1]);
+        Config::set('corppass-login.login_scopes', ['openid']);
+
+        $this->expectException(MissingConfigException::class);
+        $this->expectExceptionMessage('corppass-login.available_scopes');
+
+        ProviderConfig::corpPass();
+    }
+
+    public function test_myinfo_accepts_empty_scope_lists(): void
+    {
+        Config::set('myinfo.discovery_endpoint', 'https://example.com/discovery');
+        Config::set('myinfo.client_id', 'myinfo-client-id');
+        Config::set('myinfo.redirect_uri', 'https://example.com/myinfo-callback');
+        Config::set('myinfo.domain', 'https://example.com');
+        Config::set('myinfo.available_scopes', []);
+        Config::set('myinfo.login_scopes', null);
+
+        $config = ProviderConfig::singPassMyInfo();
+
+        $this->assertSame([], $config->availableScopes);
+        $this->assertSame([], $config->loginScopes);
+    }
+
+    public function test_myinfo_throws_when_login_scopes_contains_non_string(): void
+    {
+        Config::set('myinfo.discovery_endpoint', 'https://example.com/discovery');
+        Config::set('myinfo.client_id', 'myinfo-client-id');
+        Config::set('myinfo.redirect_uri', 'https://example.com/myinfo-callback');
+        Config::set('myinfo.domain', 'https://example.com');
+        Config::set('myinfo.available_scopes', ['openid']);
+        Config::set('myinfo.login_scopes', [['nested' => 'bad']]);
+
+        $this->expectException(MissingConfigException::class);
+        $this->expectExceptionMessage('myinfo.login_scopes');
+
+        ProviderConfig::singPassMyInfo();
+    }
 }
